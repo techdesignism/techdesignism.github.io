9ae524f8aaeee5c79e7a8e9dafd37510a710661e
bennywongysdhk <109790237+techdesignism@users.noreply.github.com>
2022-08-02 10:51:12 +0800

    update

---
+++ n/sitemap.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://techdesignism.com/sitemap')
+
+
+# In[30]:
+
+
+for x in r.text.split('\n'):
+    if '?n=' in x:
+        id = x.split('?n=')[1]
+        rx = requests.get('https://techdesignism.com/?n='+id)
+        soup = BeautifulSoup(rx.text, "lxml")
+        title = (soup.title.text+' | Techdesignism')
+        meta = soup.find_all('meta')
+        for y in meta:
+            if('property' in y.attrs):
+                if(y.attrs['property'] == 'og:description'):
+                    description = (y.attrs['content'])
+                if(y.attrs['property'] == 'og:image'):
+                    image = (y.attrs['content'])
+        outhtml = (
+f"""
+<script>
+window.location.href = "{x}";
+</script>
+<html>
+<head>
+<title>{title}</title>
+
+<meta property="og:image" content="{image}" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+
+</head>
+<body>
+{description}
+</body>
+</html>
+""")
+        with open(str(id)+'.html', 'w') as f:
+            f.write(outhtml)
+
+
+# In[31]:
+
+
+sitemaptxt = ''
+for x in r.text.split('\n'):
+    if '?n=' in x:
+        id = x.split('?n=')[1]
+        sitemaptxt += (f"https://techdesignism.github.io/n/{id}.html\n")
+
+
+# In[32]:
+
+
+with open('sitemap.txt', 'w') as f:
+    f.write(sitemaptxt)
+
+
+# In[ ]:
+
+
+
+
