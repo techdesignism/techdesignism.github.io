9aca629f8faf540b09468b65a3d32fd703175727
benny.wong <benny.wong@FLIPS.DIGITAL>
2024-11-20 12:26:09 +0800

    upload c2f

---
+++ c2f/precache-manifest.537cefdf020303037a6931710ce83823.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d044895205cf29dd15695cdbb2655451",
+    "url": "/c2f/index.html"
+  },
+  {
+    "revision": "59abee2501953803ea1b",
+    "url": "/c2f/static/css/2.0678adc1.chunk.css"
+  },
+  {
+    "revision": "ae79932492df8374e0c1",
+    "url": "/c2f/static/css/main.29f1a6fc.chunk.css"
+  },
+  {
+    "revision": "59abee2501953803ea1b",
+    "url": "/c2f/static/js/2.d035d3f4.chunk.js"
+  },
+  {
+    "revision": "ae79932492df8374e0c1",
+    "url": "/c2f/static/js/main.a7c79268.chunk.js"
+  },
+  {
+    "revision": "5bd4ac21a90e18c441ca",
+    "url": "/c2f/static/js/runtime~main.450d0b2a.js"
+  },
+  {
+    "revision": "0ea8229b841487b599fb310da4890990",
+    "url": "/c2f/static/media/Ostrich Regular.0ea8229b.ttf"
+  },
+  {
+    "revision": "5857ad01162dc00129c541ef5efbaa48",
+    "url": "/c2f/static/media/logo.5857ad01.png"
+  }
+]);
